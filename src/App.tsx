6a34c8bd90262924b811aba7c8bb75088047a6f7
apo-star <apolle.world@gmail.com>
2024-11-06 07:05:10 -0800

    fix: remote contact information

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,6 @@ function App() {
       <Scene />
       <Overlay />
       <UploadContorls />
-      <div className="absolute top-0 text-[white] text-[50px]">
-        <p>venusdev@eclipso.email</p>
-        <p>please send mail to this email address</p>
-        <p>if you understand let me know</p>
-        <p>if you want to chatting in google chat, please send message to this gmail address</p>
-        <p>apolle.world@gmail.com</p>
-      </div>
     </div>
   );
 }
